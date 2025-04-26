6a6d93f07f87f33d4d441695ccf6df45e484fa0e
Serhiy7 <ssukalo79@gmail.com>
2025-04-26 20:16:37 +0200

    classNames

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 // import Counter from "./components/Counter";
 // import Dropdown from "./components/Dropdown";
-import ColorPicker from "./components/ColorPicker";
+// import ColorPicker from "./components/ColorPicker";
 import TodoList from "./components/TodoList/";
 import { Component } from "react";
-import Form from "./components/Form/Form.jsx";
+// import Form from "./components/Form/Form.jsx";
 import initialTodos from "./todos.json";
 
-const colorPickerOptions = [
-  { label: "red", color: "#F44336" },
-  { label: "green", color: "#4CAF50" },
-  { label: "blue", color: "#2196F3" },
-  { label: "grey", color: "#607D8B" },
-  { label: "pink", color: "#E91E63" },
-  { label: "indigo", color: "#3F51B5" },
-];
+// const colorPickerOptions = [
+//   { label: "red", color: "#F44336" },
+//   { label: "green", color: "#4CAF50" },
+//   { label: "blue", color: "#2196F3" },
+//   { label: "grey", color: "#607D8B" },
+//   { label: "pink", color: "#E91E63" },
+//   { label: "indigo", color: "#3F51B5" },
+// ];
 
 class App extends Component {
   state = {
@@ -26,20 +26,37 @@ class App extends Component {
     }));
   };
 
-  formSubmitHandler = (data) => {
-    setTimeout(() => {
-      console.log(data);
-    }, 2000);
+  toggleCompleted = (todoId) => {
+    console.log(todoId);
+
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === todoId) {
+          console.log("Нашли тот туду который нужно!");
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
   };
 
-  render() {
-    // const { todos } = this.state;
+  // formSubmitHandler = (data) => {
+  //   setTimeout(() => {
+  //     console.log(data);
+  //   }, 2000);
+  // };
 
-    // const totalTodoCount = todos.length;
-    // const completedTodoCount = todos.reduce(
-    //   (total, todo) => (todo.completed ? total + 1 : total),
-    //   0
-    // );
+  render() {
+    const { todos } = this.state;
+
+    const totalTodoCount = todos.length;
+    const completedTodoCount = todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0
+    );
 
     return (
       <>
@@ -49,14 +66,18 @@ class App extends Component {
 
         {/* <Dropdown /> */}
 
-        <ColorPicker options={colorPickerOptions} />
+        {/* <ColorPicker options={colorPickerOptions} /> */}
 
-        {/* <div>
+        <div>
           <p>Общее кол-во: {totalTodoCount}</p>
           <p>Кол-во выполненных: {completedTodoCount}</p>
         </div>
 
-        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} /> */}
+        <TodoList
+          todos={todos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
       </>
     );
   }
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import classNames from "classnames";
 import "./TodoList.css";
 
-const TodoList = ({ todos, onDeleteTodo }) => (
+const TodoList = ({ todos, onDeleteTodo, onToggleCompleted }) => (
   <ul>
-    {todos.map(({ id, text }) => (
-      <li key={id} className="TodoList__item">
+    {todos.map(({ id, text, completed }) => (
+      <li
+        key={id}
+        className={classNames("TodoList__item", {
+          "TodoList__item--completed": completed,
+        })}
+      >
+        <input
+          type="checkbox"
+          className="TodoList__checkbox"
+          checked={completed} // в момент клика Браузер сам «переключает» внутреннее свойство input.checked на противоположное (из false → true, или наоборот).
+          onChange={() => onToggleCompleted(id)} // браузер генерирует событие и React вызывает  обработчик
+        />
+
         <p className="TodoList__text">{text}</p>
-        <button onClick={() => onDeleteTodo(id)}>Удалить</button>
+        <button
+          type="button"
+          className="TodoList__btn"
+          onClick={() => onDeleteTodo(id)}
+        >
+          Удалить
+        </button>
       </li>
     ))}
   </ul>
